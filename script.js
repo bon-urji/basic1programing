7ec510fedf0c331bd53b9ccdb0645b2d1a479305
bonsa <bonsa tamasgen11@gmail.com>
2025-12-25 15:22:47 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("programming ko jalqabaa jalqabeere");
+let maqaa ="boonii";
+let umrii =  13;
+let biyyaKoo = "Ethiopia";
+console.log("ani maqaan koo:",maqaa,"dha.","umriin koo:",umrii, "lafan itti dhaladhe:",biyyaKoo);
+// calculation
+let umriinKeeWaggaDhufu = umrii+1;
+let umriinKonkolaachisuu = 18-umriinKeeWaggaDhufu;
+console.log("waggaa dhufu umurin koo",umriinKeeWaggaDhufu ,"ta'a");
+console.log("konkolaachisuu eegaluuf",umriinKonkolaachisuu , "siif hafa");
